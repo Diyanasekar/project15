fd54bf15fcd056a16136acf52fb52b3e3c30ef09
Diyanasekar <diyanas1997@gmail.com>
2017-07-05 11:04:49 +0530

    Create Oddno.java

---
+++ Oddno.java
@@ -0,0 +1,26 @@
+# project15
+import java.io.*;
+import java.util.*;
+public class Oddno
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    int n;
+    n=sc.nextInt();
+    for(int i=1;i<n;i++)
+    {
+      if(i%2!=0)
+      {
+          
+          System.out.println(i);
+          }
+          }
+          }
+          }
+
+  
+
+     
